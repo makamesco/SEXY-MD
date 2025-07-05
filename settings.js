adc4ce9122ef1d715946d209a055c170c38e244a
makamesco <Makamescodigitalsolutions@gmail.com>
2025-07-05 17:45:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "SEXY-XMD",
   botOwner: 'MAKAMESCO', // Your name
-  ownerNumber: '263710407389', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254769995625', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
